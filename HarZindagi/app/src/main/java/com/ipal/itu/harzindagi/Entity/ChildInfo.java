5d4a64f3d5732876c415067545afa2867681cf30
Ali Imran <ali707one@hotmail.com>
2016-01-13 15:18:52 +0500

    folders added

---
+++ HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Entity/ChildInfo.java
@@ -0,0 +1,12 @@
+package com.ipal.itu.harzindagi.Entity;
+
+/**
+ * Created by Ali on 1/13/2016.
+ */
+public class ChildInfo {
+    public String  name;
+    public String  fatherName;
+    public String  dob;
+    public String  nic;
+    public String  phoneNum;
+}
