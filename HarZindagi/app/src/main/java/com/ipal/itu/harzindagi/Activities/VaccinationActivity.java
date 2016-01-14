b64545130c8e493c80bc354ff3391867d8e18081
Ali Imran <ali707one@hotmail.com>
2016-01-14 15:38:18 +0500

    initial layouts and java files added

--- HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/VaccinationActivity.java
+++ HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/VaccinationActivity.java
@@ -5,18 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ipal.itu.harzindagi.R;
 
-public class MainActivity extends AppCompatActivity {
+public class VaccinationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
---
+++ HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Fragments/VaccinationCFragment.java
@@ -0,0 +1,18 @@
+package com.ipal.itu.harzindagi.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ali on 1/14/2016.
+ */
+public class VaccinationCFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
