80e166737be306d6a3eae551b7bc857effdab416
Rao Shaheryar Khan <raoshaheryarkhan@gmail.com>
2016-01-21 12:58:02 +0500

    file merge

--- HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/LoginActivity.java
+++ HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.ipal.itu.harzindagi.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,22 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.ipal.itu.harzindagi.R;
 
 public class LoginActivity extends AppCompatActivity {
-
-    EditText userName;
-    EditText password;
-    String UserName;
-    String Password;
-    TextView unionCouncil;
-    TextView validator;
-    Button forgetButton;
-    Button checkInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +20,12 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        userName = ( EditText ) findViewById(R.id.loginActivityUserName);
-        UserName = userName.getText().toString();
-
-        password = ( EditText ) findViewById(R.id.loginActivityPassword);
-        Password = password.getText().toString();
-
-        //TODO: get location in SplashActivity ans pass on to LoginActivity to set this TextView
-        unionCouncil = ( TextView ) findViewById(R.id.loginActivityUnionCouncil);
-
-        validator = ( TextView ) findViewById(R.id.loginActivityValidationText);
-        validator.setText(R.string.loginValidation);
-
-        forgetButton = ( Button ) findViewById(R.id.loginActivityForgetButton);
-        forgetButton.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, R.string.loginValidation , Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-
-        checkInButton = ( Button ) findViewById(R.id.loginActivityCheckInButton);
-        checkInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "UserName: " + UserName + " , Password: " + Password , Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                startActivity(new Intent(LoginActivity.this , HomeActivity.class));
             }
         });
     }
