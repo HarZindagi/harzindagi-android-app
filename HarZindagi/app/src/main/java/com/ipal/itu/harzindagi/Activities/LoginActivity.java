857b38541e1fe42b5c9e680d295b6a750e8c983b
Ali Imran <ali707one@hotmail.com>
2016-02-25 00:24:12 +0500

    menus added

--- HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/LoginActivity.java
+++ HarZindagi/app/src/main/java/com/ipal/itu/harzindagi/Activities/LoginActivity.java
@@ -356,7 +356,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.login_main, menu);
         return true;
     }
 
